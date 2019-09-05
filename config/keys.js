2d3366fb5b97cac51c837a053e9d3a566f389c4a
Sean Boyle <sean94@gmail.com>
2019-09-05 14:15:14 +0100

    Created the login route that sends back a web token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoAtlasURI:'mongodb+srv://admin123:admin123@cluster0-nrsle.mongodb.net/test?retryWrites=true&w=majority'
+  mongoAtlasURI:'mongodb+srv://admin123:admin123@cluster0-nrsle.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey:'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -59,6 +59,50 @@ router.post('/register', (req, res) => {
   // console.log(req.body);
   
 });
+router.post('/login', (req, res) => {
+  // const { errors, isValid } = validateLoginInput(req.body);
+
+  // // Check Validation
+  // if (!isValid) {
+  //   return res.status(400).json(errors);
+  // }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      // errors.email = 'User not found';
+      return res.status(404).json({email:'User Not Found'});
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          }
+        );
+      } else {
+        // errors.password = 'Password incorrect';
+        return res.status(400).json({passport:'Passport Incorrect'});
+      }
+    });
+  });
+});
 
 
 module.exports = router;
